243fe645361dff21f11bb0170898ba6a099a7670
pedrosis <pedrosismoto@gmail.com>
2026-03-15 15:59:00 -0300

    Começando deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/camadalimite-flow-site/",
+
   server: {
     host: "::",
     port: 8080,
